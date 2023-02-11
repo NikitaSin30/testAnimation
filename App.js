2c06b3039b20bdc0e724d78a90d73a669cbe08ef
Nikita Sinelnikov <nikita.sinelnikov@lenta.ru>
2023-02-11 11:30:52 +0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
+import LottieView from 'lottie-react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi</Text>
+        <LottieView source={require('./animation.json')} autoPlay>
+        </LottieView>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems:'center',
+    justifyContent:'center',
   },
 });
